521b709eebb1f96e1bb012fc2e7d0c833098f3e4
Mads Nørgaard <madds.noergaard@gmail.com>
2026-09-21 23:27:54 +0200

    Add media controls and lyrics drawer

--- preload.js
+++ preload.js
@@ -6,10 +6,12 @@ contextBridge.exposeInMainWorld('electronAPI', {
   showInFolder: filePath => ipcRenderer.invoke('music:show-in-folder', filePath),
   startExternalDrag: filePath => ipcRenderer.send('music:start-external-drag', filePath),
   fileUrl: filePath => ipcRenderer.invoke('music:file-url', filePath),
+  findLyrics: track => ipcRenderer.invoke('music:find-lyrics', track),
   writeTags: values => ipcRenderer.invoke('music:write-tags', values),
   minimizeWindow: () => ipcRenderer.invoke('window:minimize'),
   toggleMaximizeWindow: () => ipcRenderer.invoke('window:toggle-maximize'),
   closeWindow: () => ipcRenderer.invoke('window:close'),
+  onPlaybackCommand: callback => { const listener=(_event,command)=>callback(command);ipcRenderer.on('playback:command',listener);return ()=>ipcRenderer.removeListener('playback:command',listener); },
   onWindowMaximized: callback => { const listener=(_event,maximized)=>callback(maximized);ipcRenderer.on('window:maximized',listener);return ()=>ipcRenderer.removeListener('window:maximized',listener); },
   syncToPhone: (request, onProgress) => { const listener=(_event,progress)=>onProgress?.(progress);ipcRenderer.on('music:sync-progress',listener);return ipcRenderer.invoke('music:sync-to-phone',request).finally(()=>ipcRenderer.removeListener('music:sync-progress',listener)); }
 });
